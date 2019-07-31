68abf3141afc48f86ed9c62dcbe65cd9071c324c
Nicolas Kowenski <nico@garufa.ethz.ch>
2019-07-31 16:44:06 +0200

    add pvc deletion

--- deleteall.sh
+++ deleteall.sh
@@ -4,3 +4,10 @@ helm delete --purge opennebula
 kubectl delete secret -n opennebula opennebula-ssh-keys
 kubectl delete secret -n opennebula opennebula-serveradmin
 kubectl delete role opennebula-api-access -n opennebula
+read -p "Delete PVC (persistence) for mysql? [Yy/Nn] " -n 1 -r
+echo   
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo 'Deleting PVC opennebula-mysql '
+    kubectl delete pvc -n opennebula opennebula-mysql
+fi
